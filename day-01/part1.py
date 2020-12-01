93ca8964024b0128ac6a5bb2683335051fb3fab2
Kristi Achilleos <krispy2009@gmail.com>
2020-12-01 09:18:40 +0200

    Day 1 part 2

--- day-01/part1.py
+++ day-01/part1.py
@@ -6,14 +6,24 @@ def get_input():
     return report
 
 
-def calculate_sums(report):
+def calculate_sums_for_two(report):
     for x in report:
         for y in report[1:]:
             if x + y == 2020:
                 return (x, y)
 
 
+def calculate_sums_for_three(report):
+    for x in report:
+        for y in report[1:]:
+            for z in report[2:]:
+                if x + y + z == 2020:
+                    return (x, y, z)
+
+
 if __name__ == "__main__":
     report = get_input()
-    x, y = calculate_sums(report)
-    print(f"Answer: {x*y}")
+    x, y = calculate_sums_for_two(report)
+    print(f"Part 1 Answer: {x*y}")
+    x, y, z = calculate_sums_for_three(report)
+    print(f"Part 2 Answer: {x*y*z}")
