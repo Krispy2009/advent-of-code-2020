538050c3499f8f5bd9071f70c79f1d1849efa5f8
Kristi Achilleos <krispy2009@gmail.com>
2020-12-06 11:10:36 +0200

    Day 6 part 2

--- day-06/parts.py
+++ day-06/parts.py
@@ -1,3 +1,6 @@
+from collections import Counter
+
+
 def read_input(filename):
     data = []
     full_group = ""
@@ -5,7 +8,7 @@ def read_input(filename):
         for line in f.readlines():
             if line != "\n":
                 line = line.replace("\n", "")
-                full_group = f"{full_group}{line}"
+                full_group = f"{full_group}{line}$"
             else:
                 data.append(full_group)
                 full_group = ""
@@ -14,14 +17,28 @@ def read_input(filename):
     return data
 
 
-def count_answers(data):
+def count_anyone_answers(data):
     total = 0
     for line in data:
         unique_ans = set(line)
+        unique_ans.discard("$")
         total += len(unique_ans)
+    print(total)
+
+
+def count_everyone_answers(data):
+    total = 0
+    for line in data:
+        counts = Counter(line)
+        for item in counts:
+            if counts["$"] == counts[item]:
+                total += 1
+        # remove the count for $
+        total -= 1
     print(total)
 
 
 if __name__ == "__main__":
     data = read_input("input.txt")
-    count_answers(data)
+    count_anyone_answers(data)
+    count_everyone_answers(data)
