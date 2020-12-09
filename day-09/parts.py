eea522f10b454815ede022caa710bbed63bfe8ea
Kristi Achilleos <krispy2009@gmail.com>
2020-12-09 11:20:12 +0200

    Day 9 part 2

--- day-09/parts.py
+++ day-09/parts.py
@@ -25,9 +25,32 @@ def check_numbers(data):
         number_to_check = data[i + window_size]
         if number_to_check not in make_sums(data, i, i + window_size):
             print(f"OH NO - Wrong number: {number_to_check}")
-            break
+            return number_to_check
+
+
+def find_first_biggest_number(data, number):
+    for idx, i in enumerate(data):
+        if i >= number:
+            return idx - 1
+
+
+def find_contiguous_numbers_that_sum(data, number):
+    high = 1
+    idx_to_stop_at = find_first_biggest_number(data, number)
+    for low in range(0, idx_to_stop_at + 1):
+        high = 1
+        while high <= idx_to_stop_at:
+            if sum(data[low:high]) == number:
+                numbers = sorted(data[low:high])
+                print(f"smallest: {numbers[0]}")
+                print(f"largest: {numbers[-1]}")
+                print(f"sum: {numbers[0] + numbers[-1]}")
+                return numbers[0] + numbers[-1]
+            elif sum(data[low:high]) != number:
+                high += 1
 
 
 if __name__ == "__main__":
     data = read_input("input.txt")
-    check_numbers(data)
+    number = check_numbers(data)
+    find_contiguous_numbers_that_sum(data, number)
