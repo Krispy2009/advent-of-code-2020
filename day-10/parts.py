157fd82b65715ab9c84db9d162fd4d3fa601ae23
Kristi Achilleos <krispy2009@gmail.com>
2020-12-10 23:42:41 +0200

    Day 10 part 2

--- day-10/parts.py
+++ day-10/parts.py
@@ -27,6 +27,32 @@ def find_joltage_steps(data):
     print(f"Answer: {joltage_steps[1] * joltage_steps[3]}")
 
 
+def calculate_arrangements(data):
+    # prepare adaptors
+    data = [0] + sorted(data)
+    # data.append(max(data) + 3)
+
+    possible_choices = {}
+
+    for jolt in data:
+        valid_jolts = [jolt + 1, jolt + 2, jolt + 3]
+        valid_jolts = [x for x in valid_jolts if x in data]
+        possible_choices[jolt] = valid_jolts
+    print(possible_choices)
+
+    results = {0: 1}
+    for adapter, choices in possible_choices.items():
+        print(f"adapter: {adapter}")
+        for choice in choices:
+            if choice in results:
+                results[choice] += results[adapter]
+            else:
+                results[choice] = results[adapter]
+        print(results)
+    print(results[max(results.keys())])
+
+
 if __name__ == "__main__":
-    data = read_input("input.txt")
+    data = read_input("example.txt")
     find_joltage_steps(data)
+    calculate_arrangements(data)
