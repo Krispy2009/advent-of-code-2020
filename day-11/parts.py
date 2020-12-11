b81c269302ca8fdef321e9a0f54e3b94546f3888
Kristi Achilleos <krispy2009@gmail.com>
2020-12-11 23:23:53 +0200

    Day 11 part 1

---
+++ day-11/parts.py
@@ -0,0 +1,69 @@
+import copy
+import sys
+from pprint import pprint
+
+
+def read_input(filename):
+    data = []
+    with open(filename) as f:
+        for line in f.readlines():
+            data.append(list(line.replace("\n", "")))
+    return data
+
+
+def process_steps(data):
+    temp_step = copy.deepcopy(data)
+    print_layout(temp_step)
+    for row_idx, row in enumerate(data):
+        for seat_idx, seat in enumerate(row):
+            new_seat = check_seat(data, row_idx, seat_idx, seat)
+            temp_step[row_idx][seat_idx] = new_seat
+    return temp_step
+
+
+def check_seat(data, x, y, seat_status):
+    seats_occupied = 0
+    if seat_status == ".":
+        return "."
+    # print(f"checking {x,y} which is currently {seat_status}")
+
+    for (a, b) in (
+        (x - 1, y),
+        (x + 1, y),
+        (x, y - 1),
+        (x, y + 1),
+        (x - 1, y - 1),
+        (x + 1, y + 1),
+        (x + 1, y - 1),
+        (x - 1, y + 1),
+    ):
+        if a < 0 or b < 0:
+            continue
+        try:
+            if data[a][b] == "#":
+                seats_occupied += 1
+        except IndexError:
+            pass
+        # print(f"checking outside bounds {a,b}")
+
+    if seat_status == "#" and seats_occupied >= 4:
+        return "L"
+
+    elif seat_status == "L" and seats_occupied == 0:
+        return "#"
+    return seat_status
+
+
+def print_layout(data):
+    print("\n".join(["".join(i) for i in data]))
+    print()
+
+
+if __name__ == "__main__":
+    data = read_input("input.txt")
+    while True:
+        new_data = process_steps(data)
+        if new_data == data:
+            break
+        data = copy.deepcopy(new_data)
+    print("".join(["".join(i) for i in new_data]).count("#"))
