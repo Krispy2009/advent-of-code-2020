64ed1969477286eeebbabb392eff37510fc412b4
Kristi Achilleos <krispy2009@gmail.com>
2020-12-12 11:23:13 +0200

    Day 12 part 2

--- day-12/parts.py
+++ day-12/parts.py
@@ -42,6 +42,64 @@ def find_route(data):
     print(result_east + result_north)
 
 
+def change_waypoint_direction(waypoint_directions, dir, num):
+    turn = int(num / 90)
+    if dir == "L":
+        turn = turn * -1
+
+    if turn in [1, -3]:
+        return {
+            "east": waypoint_directions["north"],
+            "west": waypoint_directions["south"],
+            "north": waypoint_directions["west"],
+            "south": waypoint_directions["east"],
+        }
+
+    if turn in [2, -2]:
+        return {
+            "east": waypoint_directions["west"],
+            "west": waypoint_directions["east"],
+            "north": waypoint_directions["south"],
+            "south": waypoint_directions["north"],
+        }
+    if turn in [3, -1]:
+        return {
+            "east": waypoint_directions["south"],
+            "west": waypoint_directions["north"],
+            "north": waypoint_directions["east"],
+            "south": waypoint_directions["west"],
+        }
+
+
+def find_route_with_waypoint(data):
+    ship_directions = {"east": 0, "west": 0, "north": 0, "south": 0}
+    waypoint_directions = {"east": 10, "west": 0, "north": 1, "south": 0}
+    for instr in data:
+        dir, num = instr[0], int(instr[1:])
+        if dir == "F":
+            for d in ship_directions:
+                ship_directions[d] += num * waypoint_directions[d]
+        if dir == "N":
+            waypoint_directions["north"] += num
+        if dir == "E":
+            waypoint_directions["east"] += num
+        if dir == "S":
+            waypoint_directions["south"] += num
+        if dir == "W":
+            waypoint_directions["west"] += num
+        if dir in ["R", "L"]:
+            waypoint_directions = change_waypoint_direction(waypoint_directions, dir, num)
+
+        print(ship_directions)
+        print(waypoint_directions)
+        print()
+
+    result_east = abs(ship_directions["east"] - ship_directions["west"])
+    result_north = abs(ship_directions["south"] - ship_directions["north"])
+    print(result_east + result_north)
+
+
 if __name__ == "__main__":
     data = read_input("input.txt")
     find_route(data)
+    find_route_with_waypoint(data)
