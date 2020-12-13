1f892b9b1be0842a5720f05771f44c81c2ca7e21
Kristi Achilleos <krispy2009@gmail.com>
2020-12-13 21:20:19 +0200

    Day 13 part 2

--- day-13/parts.py
+++ day-13/parts.py
@@ -1,8 +1,23 @@
-input_str = """1000053
-19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,547,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17"""
-
 example_str = """939
 7,13,x,x,59,x,31,19"""
+
+example_str = """939
+17,x,13,19"""
+
+example_str = """939
+67,7,59,61"""
+
+example_str = """939
+67,x,7,59,61"""
+
+example_str = """939
+67,7,x,59,61"""
+
+example_str = """939
+1789,37,47,1889"""
+
+input_str = """1000053
+19,x,x,x,x,x,x,x,x,x,x,x,x,37,x,x,x,x,x,523,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,x,13,x,x,x,x,23,x,x,x,x,x,29,x,547,x,x,x,x,x,x,x,x,x,41,x,x,x,x,x,x,17"""
 
 
 def calc_first_future_bus(timestamp, bus):
@@ -11,12 +26,7 @@ def calc_first_future_bus(timestamp, bus):
     return last_bus + bus
 
 
-if __name__ == "__main__":
-    timestamp, buses = input_str.split("\n")
-    timestamp = int(timestamp)
-    buses = [int(i) for i in buses.split(",") if i != "x"]
-    print(timestamp, buses)
-
+def part_1(timestamp, buses):
     closest_bus = None
     time_diff = None
 
@@ -30,3 +40,34 @@ if __name__ == "__main__":
     print(f"Closest bus: {closest_bus} - time diff: {time_diff}")
     print(f"Answer: {closest_bus * time_diff}")
 
+
+def part_2(buses_list):
+    buses = {k: v for v, k in enumerate(buses_list)}
+    if "x" in buses:
+        del buses["x"]
+    last_bus = buses_list[-1]
+    buses_list = [i for i in buses_list if i != "x"]
+    multiplier = 1
+    timestamp = 100_000_000_000_000
+    while buses_list:
+        for bus in buses_list[:]:
+            if ((timestamp) + buses[bus]) % bus != 0:
+                timestamp += multiplier
+                break
+            else:
+                buses_list.remove(bus)
+                multiplier *= bus
+
+            if bus == last_bus:
+                print(timestamp)
+                return timestamp
+
+
+if __name__ == "__main__":
+    timestamp, buses = input_str.split("\n")
+    timestamp = int(timestamp)
+    all_buses = [int(i) if i != "x" else i for i in buses.split(",")]
+    buses = [int(i) for i in buses.split(",") if i != "x"]
+
+    part_1(timestamp, buses)
+    part_2(all_buses)
