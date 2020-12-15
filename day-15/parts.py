3330e6c46eacad3fa0f2b3617e676c9de11e5456
Kristi Achilleos <krispy2009@gmail.com>
2020-12-15 12:47:49 +0200

    Day 15 part 2

--- day-15/parts.py
+++ day-15/parts.py
@@ -20,25 +20,25 @@ def play_game(numbers):
         turn += 1
     while True:
 
-        print(f"Turn {turn}: {dict(stats)}")
-        print(f"last number checked: {last_number_checked}")
+        # print(f"Turn {turn}: {dict(stats)}")
+        # print(f"last number checked: {last_number_checked}")
         num = last_number_checked
         if len(stats[last_number_checked]) == 1:
             num = 0
-            print(f"last num: {last_number_checked}.. num is now {num}")
+            # print(f"last num: {last_number_checked}.. num is now {num}")
             last_number_checked = num
             stats[num].append(turn)
             turn += 1
-            print()
+            # print()
             continue
 
         if num in stats:
             if len(stats[num]) + 1 >= 2:
-                print(
-                    f"Num {num} has been spoken before in turns {stats[num][-1]} and  {stats[num][-2]}"
-                )
+                # print(
+                #     f"Num {num} has been spoken before in turns {stats[num][-1]} and  {stats[num][-2]}"
+                # )
                 new_num = stats[num][-1] - stats[num][-2]
-                print(f"new number = {new_num}")
+                # print(f"new number = {new_num}")
                 stats[new_num].append(turn)
                 last_number_checked = new_num
             else:
@@ -49,10 +49,10 @@ def play_game(numbers):
             stats[num] = [turn]
             last_number_checked = num
         turn += 1
-        print()
-        if turn > 2020:
+        # print()
+        if turn > 30_000_000:
             for nums, turns in stats.items():
-                if 2020 in turns:
+                if 30_000_000 in turns:
                     print(nums)
             return
 
