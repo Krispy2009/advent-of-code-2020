958150e2c7369f02bfa11cbe8e8ecc520fc934a6
Kristi Achilleos <krispy2009@gmail.com>
2020-12-19 13:46:55 +0200

    Day 18 part 2

--- day-18/parts.py
+++ day-18/parts.py
@@ -8,7 +8,28 @@ def read_input(filename):
         return f.read().splitlines()
 
 
-def calculate(problem):
+def calculate_part_2(problem):
+    parts = problem.split()
+    new_parts = parts[:]
+    while "+" in parts:
+        # print(new_parts)
+        addition = new_parts.index("+")
+        new_val = int(new_parts[addition - 1]) + int(new_parts[addition + 1])
+        new_parts.pop(addition - 1)
+        new_parts.pop(addition - 1)
+        new_parts.pop(addition - 1)
+        new_parts.insert(addition - 1, str(new_val))
+        parts = new_parts[:]
+
+    print(parts)  # should all be * now
+    total = int(parts[0])
+    for p in range(1, len(parts), 2):
+        total *= int(parts[p + 1])
+
+    return total
+
+
+def calculate_part_1(problem):
     parts = problem.split()
     total = int(parts[0])
 
@@ -27,13 +48,13 @@ def solve_problem(problem):
     while match:
         res = 0
         groups = match.groups()
-        res += calculate(groups[0])
+        res += calculate_part_2(groups[0])
         problem = problem.replace(f"({groups[0]})", str(res))
         match = re.search(parenthesis_re, problem)
 
     else:
         # print(f"no parenthesis found in {problem}")
-        total += calculate(problem)
+        total += calculate_part_2(problem)
 
     return total
 
@@ -43,7 +64,7 @@ if __name__ == "__main__":
     homework = read_input("input.txt")
     for problem in homework:
         res = solve_problem(problem)
-        # print(f"****** {problem} = {res}")
+        print(f"------>     {problem} = {res}")
         result += res
 
     print(result)
